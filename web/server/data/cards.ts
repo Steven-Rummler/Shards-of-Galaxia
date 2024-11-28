9180cab930df86b369eee27b79dde83c8ca519bb
Steven Rummler <steven.rummler@gmail.com>
2024-11-28 15:54:41 -0700

    add cards

---
+++ web/server/data/cards.ts
@@ -0,0 +1,6 @@
+import type { Card } from "../model/card";
+
+export const cards: Card[] = [
+  { title: 'Forage', text: 'Search for resources in your territory.', type: 'action' },
+  { title: 'Raid', text: 'Send a raiding party to steal resources from another player.', type: 'interaction' },
+];
--- web/server/model/player.ts
+++ web/server/model/player.ts
@@ -1,7 +1,18 @@
+import type { Card } from "./card";
+
 export class Player {
   name: string;
+  hand: Card[];
 
   constructor(name: string) {
     this.name = name;
+    this.hand = [];
+  }
+  addCard(card: Card) {
+    this.hand.push(card);
+  }
+  playCard(cardIndex: number) {
+    const card = this.hand.splice(cardIndex, 1)[0];
+    return card;
   }
 }
